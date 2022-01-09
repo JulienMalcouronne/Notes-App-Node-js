65cda4a2f267feaf583adbf28574f00c6b06a87c
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-09 23:25:35 +0100

    done with some json manipulation

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,22 @@
-const book = {
-  title: 'Ego is the Ennemy',
-  author: 'Ryan Holiday'
-}
+const fs = require('fs')
 
-const bookJSON = JSON.stringify(book)
-console.log(bookJSON);
+// const book = {
+//   title: 'Ego is the Ennemy',
+//   author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+const datajson = fs.readFileSync('1-json.json')
+const dataJSON = datajson.toString()
+const data = JSON.parse(dataJSON)
+data.name = 'Julien'
+data.age = 28
+const datastring = JSON.stringify(data)
+fs.writeFileSync('1-json.json', datastring)
