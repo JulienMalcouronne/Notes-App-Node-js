717ffa2df16ff79337c9644d249e8545af8b12fd
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-09 22:22:06 +0100

    parse process.argv

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
+console.log(process.argv)
 
-console.log(chalk.bold.green("success"))
+if (command === 'add') {
+  console.log('Adding node')
+} else if (command === 'remove') {
+  console.log('Removing note!')
+}
+// const msg = getNotes()
+// console.log(msg)
+
+// console.log(chalk.red("error"))
+
+// console.log(process.argv[2])
 
 // const add = require('./utils.js')
 // const sum = add(4, -2)
